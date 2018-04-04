31d221973adb332eabdb54ea4865b66984632dd9
Artur Correa <correa.dev@gmail.com>
2018-04-04 20:23:23 -0300

    
    s
    s
    asd

---
+++ src/components/CardProduto.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ toperson }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CardProduto',
+  data () {
+    return {
+      toperson: 'AEHOOOOOO'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
